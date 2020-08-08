58b027641f4616fb94333bb411f532fea40c8dc5
Javier Aguilar <coyote176@msn.com>
2020-08-08 19:09:19 +0000

    Done.

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -1,4 +1,4 @@
-def bonus
+learn submit_tagdef bonus
   epic_tragedy = {
    :montague => {
       :patriarch => {name: "Lord Montague", age: "53"},
@@ -20,9 +20,12 @@ def bonus
    }
   }
 
+epic_tragedy[:montague][:hero][:status] = "dead"
+epic_tragedy[:capulet][:heroine][:status] = "dead"
   #code your solution here:
 
-  
+ 
+ 
 
   #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
   epic_tragedy
